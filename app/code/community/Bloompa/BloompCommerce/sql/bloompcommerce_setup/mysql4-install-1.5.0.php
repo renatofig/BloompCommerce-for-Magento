e3ee39a40425cdd5a04e0890e248264db409c18b
Ricardo Dantas <ricardodantas@gmail.com>
2012-11-24 18:13:42 -0200

    added bloomp commerce code injection in the products, carts and chackout success pages.

--- app/code/community/Bloompa/BloompCommerce/sql/bloompcommerce_setup/mysql4-install-1.5.0.php
+++ app/code/community/Bloompa/BloompCommerce/sql/bloompcommerce_setup/mysql4-install-1.5.0.php
@@ -10,7 +10,7 @@ CREATE TABLE `bloompa_settings` (
   `value` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci COMMENT='Tabela com as configurações do bloompa';
-INSERT INTO bloompa_settings(id,product,param,value) VALUES(NULL,'BloompCommerce','token','none');
+INSERT INTO bloompa_settings(id,product,param,value) VALUES(NULL,'BloompCommerce','token','novo');
 SQLTEXT;
 
 $installer->run($sql);
